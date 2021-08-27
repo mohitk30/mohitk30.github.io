1d34375cc6e5b5c1c19beead90699ae2859e5065
Mohit kumar <76505347+mohitk30@users.noreply.github.com>
2021-08-27 17:55:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -69,3 +69,36 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
     if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
+
+
+
+
+
+
+// mondel
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+showmondel();
+// When the user clicks the button, open the modal 
+function showmondel() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
